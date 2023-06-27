5e006071ce57e639f2fbc73d551410a0c736675d
sawnjordan <sawnjordan@gmail.com>
2023-06-27 20:00:48 +0545

    added express middleware to receive/view the req data

--- src/config/app.js
+++ src/config/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = require("../routes");
 const app = express();
 
+//this is to get the data from req.body if your content type is application/json
+app.use(express.json());
+
+//this is to get the data from req.body if you content type is application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/v1/", router);
 
 module.exports = app;
