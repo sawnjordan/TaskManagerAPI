38bc36fdfe527d148a3477597dab3eee6435b10a
sawnjordan <sawnjordan@gmail.com>
2023-06-28 19:18:01 +0545

    added connection with Mongo DB, and created a model

---
+++ src/modules/task/task.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
--- src/server.js
+++ src/server.js
@@ -1,18 +1,32 @@
 const http = require("http");
-const express = require("express");
 const app = require("./config/app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT = 5005;
 
 const server = http.createServer(app);
 
-server.listen(PORT, (err) => {
-  if (!err) {
-    console.log(`Server listening to PORT: ${PORT}`);
-    console.log(
-      `Browser http://localhost:${PORT} OR http://127.0.0.1:${PORT} to browser the server.`
-    );
-    console.log(`Press CTRL + C to stop the server.`);
-  } else {
-    console.log(`Error connection to PORT: ${PORT}`);
-  }
-});
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    server.listen(PORT);
+  })
+  .then(() => {
+    console.log(`Server listening to PORT:${PORT} and connected to DataBase`);
+    console.log(`Browser: http://localhost:${PORT} `);
+    console.log(`Press CTRL + C to STOP the server`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// server.listen(PORT, (err) => {
+//   if (!err) {
+//     console.log(`Server listening to PORT: ${PORT}`);
+//     console.log(
+//       `Browser http://localhost:${PORT} OR http://127.0.0.1:${PORT} to browser the server.`
+//     );
+//     console.log(`Press CTRL + C to stop the server.`);
+//   } else {
+//     console.log(`Error connection to PORT: ${PORT}`);
+//   }
+// });
